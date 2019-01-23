2e4e985a10165bb24c7414726f72b966679609da
Blackscure <Wekesabuyahi@gmail.com>
2019-01-23 12:21:07 +0300

    code adedd

--- js/main.js
+++ js/main.js
@@ -91,4 +91,27 @@ function fetchBookmarks(){
   }
 }
 
- 
+ // Validate Form
+function validateForm(siteName, siteUrl){
+    if(!siteName || !siteUrl){
+      alert('Please fill in the form');
+      return false;
+    }
+  
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+  
+    if(!siteUrl.match(regex)){
+      alert('Please use a valid URL');
+      return false;
+    }
+  
+    return true;
+  }
+  
+  function addhttp(url) {
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = "http://" + url;
+    }
+    return url;
+  }
